2a2ceaaa63109dc8e3e47e4171b3426fda50c180
U-DESKTOP-9EAUVS9\LMC <lamberto9@gmail.com>
2020-01-27 18:03:25 -0600

    Tema 1.1 Conceptos fundamentales del software (dato, informacion)

---
+++ 01_Tema_1.1/src/TestFunciong.cpp
@@ -0,0 +1,13 @@
+/** TestFunciong.cpp
+ */
+#include <iostream>
+using std::cout;
+
+int
+main(){
+char g(unsigned int);
+  cout<<"g("<<5<<") devuelve "<<g(5)<<"\n";
+  return 0;
+}/*end main()*/
+
+
