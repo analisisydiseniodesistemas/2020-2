4ccb5ad54cb18dffe31b7f719d29e975ccd83411
U-DESKTOP-9EAUVS9\LMC <lamberto9@gmail.com>
2020-02-07 17:21:17 -0600

    2020.02.07.17.21

---
+++ 02_DiaDLaSemana/DFecha.h
@@ -0,0 +1,8 @@
+/**DFecha.h
+ */
+struct Fecha{
+  unsigned int d,m,a;  /*dia,mes,an\~{n}o*/
+};/*end struct Fecha*/
+string get_string_fecha(struct Fecha F);
+
+
---
+++ 02_DiaDLaSemana/main.cpp
@@ -0,0 +1,13 @@
+/**main.cpp
+ */
+#include <iostream>
+using namespace std;
+#include <DFecha.h>
+
+int main(int argc,char *argv[])
+{
+  struct Fecha f;
+  f.d=7;f.m=2;f.a=2020;
+  cout<<get_string_fecha(f);
+}/*end main()*/
+
